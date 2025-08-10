d2abe207ac10007e4b49cb449b22d8bb0a56a94f
Dezmont96 <dezmontmayals@gmail.com>
2025-08-10 22:37:20 +0300

    1patch

--- src/js/active-nav.js
+++ src/js/active-nav.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const currentLocation = window.location.pathname;
   const navLinks = document.querySelectorAll('.main-nav a');
 
